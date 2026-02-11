6f7d07e7b71e57cbb91dbfd6bbd0d2a7cc522dc2
Giovane Aires Gomes <77764303+giovanegomes@users.noreply.github.com>
2026-02-11 10:23:23 +0000

    adds `/api/v1/migrations`endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,35 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrationOptions = {
+    dbClient,
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: request.method === "GET",
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+
+    await dbClient.end();
+
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner(defaultMigrationOptions);
+    const status = migratedMigrations.length > 0 ? 201 : 200;
+
+    await dbClient.end();
+
+    return response.status(status).json(migratedMigrations);
+  }
+
+  return response.status(405).end;
+}
